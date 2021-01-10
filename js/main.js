06aaf5538ce14b58193b5e44f26a0a6b5a515d3b
NikitaNevsky <40342871+NikitaNevsky@users.noreply.github.com>
2021-01-11 00:43:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,4 +22,54 @@ $('.partners-slider').slick({
           }
         },
       ]
-  });
+});
+
+$('.home-reviews-slider .slider').slick({
+    arrows:false,
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '<a class="slick-arrow slick-prev"><i class="fa fa-angle-left"></i></a>',
+    nextArrow: '<a class="slick-arrow slick-next"><i class="fa fa-angle-right"></i></a>',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+      ]
+});
+
+// $('.products-slider .slider').slick({
+//     arrows:true,
+//     infinite: false,
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     prevArrow: '<a class="slick-arrow slick-prev"><i class="fa fa-angle-left"></i></a>',
+//     nextArrow: '<a class="slick-arrow slick-next"><i class="fa fa-angle-right"></i></a>',
+//     responsive: [
+//         {
+//           breakpoint: 1024,
+//           settings: {
+//             slidesToShow: 5,
+//             slidesToScroll: 1
+//           }
+//         },
+//         {
+//           breakpoint: 600,
+//           settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 1
+//           }
+//         },
+//       ]
+// });
